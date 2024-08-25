38f525281103cdac595a7c8e65c4df5934740e5b
Muhammad Raza <codewithsmr@gmail.com>
2024-08-25 15:58:13 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// Rendom Password generator JavaScript
+
+let passwordBox = document.getElementById("password");
+let length = 12;
+
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+let numbers = "0123456789";
+let symbols = "!@#$%&*()_+?={}~`[]\|";
+
+let allChars = upperCase + lowerCase + numbers + symbols;
+
+function createPassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+
+    while (length > password.length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword() {
+    passwordBox.select();
+    document.execCommand("copy");
+}
+
+document.getElementById("generateBtn").addEventListener("click", createPassword);
+
+createPassword();
